76d807e3af57c238353bcb302586b21630193ab2
Josef Hardi <josef.hardi@gmail.com>
2025-11-12 15:03:46 -0800

    style: Remove unused class

--- src/main/java/edu/stanford/protege/robot/pipeline/ProjectPipelines.java
+++
@@ -1,7 +0,0 @@
-package edu.stanford.protege.robot.pipeline;
-
-import edu.stanford.protege.webprotege.common.ProjectId;
-import java.util.List;
-
-public record ProjectPipelines(ProjectId projectId, List<RobotPipeline> pipelines) {
-}
