62d43df3a8cfbb7b73b3126638c253a777e0d111
Josef Hardi <josef.hardi@gmail.com>
2025-11-24 15:23:39 -0800

    refactor: PipelineStatus now tracks the progress of each pipeline stage

---
+++ src/main/java/edu/stanford/protege/robot/pipeline/StageStatus.java
@@ -0,0 +1,26 @@
+package edu.stanford.protege.robot.pipeline;
+
+/**
+ * Represents the execution status of a single pipeline stage.
+ */
+public enum StageStatus {
+  /**
+   * The stage is waiting to be executed.
+   */
+  WAITING,
+
+  /**
+   * The stage is currently running.
+   */
+  RUNNING,
+
+  /**
+   * The stage finished successfully without errors.
+   */
+  FINISHED_WITH_SUCCESS,
+
+  /**
+   * The stage finished with one or more errors.
+   */
+  FINISHED_WITH_ERROR
+}
