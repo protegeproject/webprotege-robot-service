d0b87bc4a0d02b7d8841699b1b531d371a73572c
Matthew Horridge <matthew.horridge@stanford.edu>
2026-02-04 16:24:32 -0800

    Add get/set pipelines handlers
    - add IPC requests/responses for pipeline fetch/save
    
    - implement handlers using pipeline repository
    
    - register handlers in application config

---
+++ src/main/java/edu/stanford/protege/robot/GetRobotPipelinesHandler.java
@@ -0,0 +1,39 @@
+package edu.stanford.protege.robot;
+
+import edu.stanford.protege.robot.pipeline.PipelineRepository;
+import edu.stanford.protege.robot.service.message.GetRobotPipelinesRequest;
+import edu.stanford.protege.robot.service.message.GetRobotPipelinesResponse;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import javax.annotation.Nonnull;
+import reactor.core.publisher.Mono;
+
+@WebProtegeHandler
+public class GetRobotPipelinesHandler
+    implements CommandHandler<GetRobotPipelinesRequest, GetRobotPipelinesResponse> {
+
+  private final PipelineRepository pipelineRepository;
+
+  public GetRobotPipelinesHandler(PipelineRepository pipelineRepository) {
+    this.pipelineRepository = pipelineRepository;
+  }
+
+  @Nonnull
+  @Override
+  public String getChannelName() {
+    return GetRobotPipelinesRequest.CHANNEL;
+  }
+
+  @Override
+  public Class<GetRobotPipelinesRequest> getRequestClass() {
+    return GetRobotPipelinesRequest.class;
+  }
+
+  @Override
+  public Mono<GetRobotPipelinesResponse> handleRequest(GetRobotPipelinesRequest request,
+                                                       ExecutionContext executionContext) {
+    var pipelines = pipelineRepository.findPipelines(request.projectId());
+    return Mono.just(new GetRobotPipelinesResponse(pipelines));
+  }
+}
---
+++ src/main/java/edu/stanford/protege/robot/service/message/GetRobotPipelinesResponse.java
@@ -0,0 +1,19 @@
+package edu.stanford.protege.robot.service.message;
+
+import static edu.stanford.protege.robot.service.message.GetRobotPipelinesRequest.CHANNEL;
+
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import edu.stanford.protege.robot.pipeline.RobotPipeline;
+import edu.stanford.protege.webprotege.common.Response;
+import java.util.List;
+import java.util.Objects;
+import javax.annotation.Nonnull;
+
+@JsonTypeName(CHANNEL)
+public record GetRobotPipelinesResponse(@Nonnull List<RobotPipeline> pipelines)
+    implements Response {
+
+  public GetRobotPipelinesResponse {
+    Objects.requireNonNull(pipelines, "pipelines cannot be null");
+  }
+}
